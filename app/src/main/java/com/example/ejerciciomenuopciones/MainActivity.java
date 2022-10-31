237a21a33fa439704b1f09b8a99c116b6d99b25e
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-31 21:50:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciomenuopciones/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.ejerciciomenuopciones;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.ejerciciomenuopciones.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Menu contextual
+        imageView1 = findViewById(R.id.idIVAsir);
+        imageView2 = findViewById(R.id.idIVDam);
+        imageView3 = findViewById(R.id.idIVDaw);
+
+        registerForContextMenu(imageView1);
+        registerForContextMenu(imageView2);
+        registerForContextMenu(imageView3);
+    }
+
+    //Menu opciones
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    //Menu contextual
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+    }
+
+    //Menu opciones
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.idAsir:
+                Intent intent1 = new Intent(this, ScrollingASIR.class);
+                startActivity(intent1);
+                finish();
+                return true;
+            case R.id.idDaw:
+                Intent intent3 = new Intent(this, ScrollingDAW.class);
+                startActivity(intent3);
+                finish();
+                return true;
+            case R.id.idDam:
+                Intent intent2 = new Intent(this, ScrollingDAM.class);
+                startActivity(intent2);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Menu contextual
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.idAsir:
+                Intent intent1 = new Intent(this, com.example.ejerciciomenuopciones.ScrollingASIR.class);
+                startActivity(intent1);
+                finish();
+                return true;
+            case R.id.idDaw:
+                Intent intent2 = new Intent(this, com.example.ejerciciomenuopciones.ScrollingDAW.class);
+                startActivity(intent2);
+                finish();
+                return true;
+            case R.id.idDam:
+                Intent intent3 = new Intent(this, com.example.ejerciciomenuopciones.ScrollingDAM.class);
+                startActivity(intent3);
+                finish();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ejerciciomenuopciones/ScrollingASIR.java
@@ -0,0 +1,43 @@
+package com.example.ejerciciomenuopciones;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.ejerciciomenuopciones.databinding.ActivityScrollingAsirBinding;
+
+public class ScrollingASIR extends AppCompatActivity {
+
+    private ActivityScrollingAsirBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingAsirBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(this, com.example.ejerciciomenuopciones.MainActivity.class);
+                //startActivity(intent);
+                //finish();
+            }
+        });
+    }
+}
